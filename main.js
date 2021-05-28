369530be3e21c937308b30dbdfcc451752c5cf88
Jason Deitz <thewanderer@Jasons-MacBook-Pro.local>
2021-05-27 21:40:19 -0700

    rough draft of all styling done

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-//          App State               //
+//          App State               //          
 
-//          
+
+
+
+
+
+
+
+
+
+
+
+
+
+const btn = document.querySelector("body > button")
+btn.addEventListener('click', begin())
+
+function begin(){
+    console.log('hello!');
+}

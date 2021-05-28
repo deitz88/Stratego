e01a2f33fa7243758cfa125e5b551d6c7ef7456d
Jason Deitz <thewanderer@Jasons-MacBook-Pro.local>
2021-05-28 08:54:36 -0700

    added css and js to seperate folders

---
+++ JS/main.js
@@ -0,0 +1,39 @@
+//         variables          //          
+// gameboard
+// messages
+// const pieces = 
+
+
+
+
+//          cashed elements    //
+
+// player 1 pieces
+// player 2 pieces
+
+
+
+
+//          state            //
+
+// location of the pieces
+// logic for understanding areas that can be moved to
+
+
+//          render          //
+
+// display moved piece
+// change msg on board for next player
+
+
+
+
+
+
+
+const btn = document.querySelector("body > button")
+btn.addEventListener('click', begin())
+
+function begin(){
+    console.log('hello!');
+}
